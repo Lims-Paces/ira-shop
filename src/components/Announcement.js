226f6701622ac0f4576e7024d13fb4b5f5eee79d
Lims-Paces <oluwapamilerin1234@gmail.com>
2023-03-22 11:34:57 +0100

    completed navbar and announcement section

---
+++ src/components/Announcement.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: rgb(126, 126, 232);
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-style: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return (
+    <div>
+      <Container>Wholesale deals! We cater for your deliveries!</Container>
+    </div>
+  );
+};
+
+export default Announcement;
---
+++ src/components/Navbar.js
@@ -0,0 +1,101 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import { Badge, ShoppingCart, ShoppingCartOutlined } from "@mui/icons-material";
+
+const Container = styled.div`
+  /* height: 60px; */
+  background-color: white;
+`;
+
+const Wrapper = styled.div`
+  padding: 0px 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.h3`
+  flex: 1;
+  font-size: 13px;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Dot = styled.span`
+  color: rgb(13, 13, 61);
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+const Center = styled.div`
+  flex: 3;
+  display: flex;
+  align-items: center;
+  margin: 0 0 0 0;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+  width: 100%;
+  border-radius: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  width: 100%;
+`;
+
+const MenuItem = styled.div`
+  font-style: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+//What i did to resolve MUI issues
+// npm install @emotion/react
+// npm install @emotion/styled
+// npm i @emotion/core
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Logo>
+            IRA <Dot>.</Dot>
+          </Logo>
+        </Left>
+        <Center>
+          <Language>EN</Language>
+          <SearchContainer>
+            <SearchIcon style={{ color: "gray", fontSize: "20px" }} />
+            <Input placeholder="Search IRA"></Input>
+          </SearchContainer>
+        </Center>
+        <Right>
+          <MenuItem>Account</MenuItem>
+          <MenuItem>Help</MenuItem>
+          <MenuItem>
+            <Badge color="secondary" badgeContent={0} showZero>
+              <ShoppingCart />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
